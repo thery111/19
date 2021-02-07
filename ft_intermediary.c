c2e6d01c4c381116b3cedd4502657f2230e5559f
thery111 <66388169+thery111@users.noreply.github.com>
2021-02-07 21:33:09 +0100

    Update ft_intermediary.c

--- ft_intermediary.c
+++ ft_intermediary.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/ft_printf.h"
+#include "ft_printf.h"
 
 void ft_intermediary(const char *format, t_printf *tab, va_list list)
 {
